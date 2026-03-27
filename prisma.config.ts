d72feaea3aae9157786fd2c4d31c9d6b5fff922e
Santiago Arias <santiari05@hotmail.com>
2026-03-26 21:36:12 -0500

    Refactor Prisma configuration to use updated paths and remove obsolete seed and schema files

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,10 @@ import { defineConfig, env } from '@prisma/config';
 import type { PrismaConfig } from 'prisma';
 
 export default defineConfig({
-  schema: 'src/core/database/schema/schema.prisma',
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: 'src/core/database/schema/migrations',
-    seed: 'ts-node -r tsconfig-paths/register ./src/core/database/seed.ts',
+    path: 'prisma/migrations',
+    seed: 'ts-node -r tsconfig-paths/register ./prisma/seed.ts',
   },
   datasource: {
     url: env('DATABASE_URL'),
