7cfe748db47b8e04713b3a32ec3353d70ceba6b9
Santiago Arias <santiari05@hotmail.com>
2026-04-11 15:30:37 -0500

    Add logging to ChatService for chat responses and refactor MsRoboticsService to use firstValueFrom for better promise handling in speak method.

--- src/ms-robotics-integration/ms-robotics.service.ts
+++ src/ms-robotics-integration/ms-robotics.service.ts
@@ -1,17 +1,17 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { ClientProxy, RpcException } from "@nestjs/microservices";
-import { catchError } from "rxjs";
+import { catchError, firstValueFrom } from "rxjs";
 import { NATS_SERVICE, ROBOTICS_MS } from "src/core/constants/ms-names.constant";
 
 @Injectable()
 export class MsRoboticsService {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
-  async speak({message}:{ message: string }) {
-    return this.client.send<void>(`${ROBOTICS_MS}.voiceService.speak`, { message }).pipe(
+  speak({message}:{ message: string }): Promise<void> {
+    return firstValueFrom(this.client.send<void>(`${ROBOTICS_MS}.voiceService.speak`, { message }).pipe(
       catchError((error) => {
-        throw new RpcException(error);
-      }),
-    );
+          throw new RpcException(error);
+        }),
+    ));
   }
 }
