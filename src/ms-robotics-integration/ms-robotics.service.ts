5f96f1b260bfda427dbb1773fec6149e84cbd93b
Santiago Arias <santiari05@hotmail.com>
2026-04-11 15:33:24 -0500

    Refactor speak method in MsRoboticsService to accept SpeakPayload for improved type safety and clarity in message handling.

--- src/ms-robotics-integration/ms-robotics.service.ts
+++ src/ms-robotics-integration/ms-robotics.service.ts
@@ -2,13 +2,14 @@ import { Inject, Injectable } from "@nestjs/common";
 import { ClientProxy, RpcException } from "@nestjs/microservices";
 import { catchError, firstValueFrom } from "rxjs";
 import { NATS_SERVICE, ROBOTICS_MS } from "src/core/constants/ms-names.constant";
+import { SpeakPayload } from "./dto/voice.dto";
 
 @Injectable()
 export class MsRoboticsService {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
-  speak({message}:{ message: string }): Promise<void> {
-    return firstValueFrom(this.client.send<void>(`${ROBOTICS_MS}.voiceService.speak`, { message }).pipe(
+  speak(speakPayload: SpeakPayload): Promise<void> {
+    return firstValueFrom(this.client.send<void>(`${ROBOTICS_MS}.voiceService.speak`, speakPayload).pipe(
       catchError((error) => {
           throw new RpcException(error);
         }),
