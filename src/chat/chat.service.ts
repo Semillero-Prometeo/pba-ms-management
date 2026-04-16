b82cb6b8e9c7c7a86bfcfd9bf35c0025aa8343d9
Santiago Arias <santiari05@hotmail.com>
2026-04-15 23:20:07 -0500

    Add logging to ChatService for message handling and remove hardcoded greeting response

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { MsRoboticsService } from 'src/ms-robotics-integration/ms-robotics.service';
 import { ChatStructuredRequestDto } from 'src/openai-integration/dto/chat-structured-request.dto';
 import { ChatStructuredResponseDto } from 'src/openai-integration/dto/chat-structured-response.dto';
@@ -6,16 +6,20 @@ import { OpenaiIntegrationService } from 'src/openai-integration/openai-integrat
 
 @Injectable()
 export class ChatService {
+  private readonly logger = new Logger(ChatService.name);
+
   constructor(
     private readonly openaiIntegrationService: OpenaiIntegrationService,
     private readonly msRoboticsService: MsRoboticsService,
   ) {}
 
   async chat(chatStructuredRequestDto: ChatStructuredRequestDto): Promise<ChatStructuredResponseDto> {
-    const response: ChatStructuredResponseDto = {
-      reply: 'Hola, soy R-One, el asistente de la Universidad Libre de Colombia. ¿En qué puedo ayudarte?',
-    };
-    return response;
+    this.logger.log("Sending message to openai integration");
+    
+    // const response: ChatStructuredResponseDto = {
+    //   reply: 'Hola, soy R-One, el asistente de la Universidad Libre de Colombia. ¿En qué puedo ayudarte?',
+    // };
+    // return response;
 
     const chatResponse = await this.openaiIntegrationService.chatStructured(chatStructuredRequestDto);
 
